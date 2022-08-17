709af0be3fe5f1e8ff0592b678b5aaa3147808e7
Lam Nguyen <nguyenquanglam3008@gmail.com>
2022-08-17 18:59:52 +0700

    Render dish list

--- core/pages/restaurants/[id].tsx
+++ core/pages/restaurants/[id].tsx
@@ -6,6 +6,11 @@ import {
   Heading,
   Spinner,
   Stack,
+  Text,
+  List,
+  ListItem,
+  Link as ChakraLink,
+  Divider,
 } from '@chakra-ui/react'
 import { Prisma } from '@prisma/client'
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
@@ -16,7 +21,9 @@ import {
   getIdPathForRestaurant,
   getRestaurantFromId,
 } from '../../libs/db/restaurant'
-import useSWR from 'swr'
+import useSWR from 'swr/immutable'
+import { Fetcher } from 'swr'
+import Link from 'next/link'
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const menuIds = await getIdPathForRestaurant()
@@ -49,11 +56,18 @@ export const getStaticProps: GetStaticProps = async (context) => {
   }
 }
 
+type GetRestaurantResult = Awaited<
+  Prisma.PromiseReturnType<typeof getRestaurantFromId>
+>
+
 type RestaurantProps = {
-  restaurant: Awaited<Prisma.PromiseReturnType<typeof getRestaurantFromId>>
+  restaurant: GetRestaurantResult
 }
 
-const fetcher = async (url: string, shopeeUrl: string | null) => {
+const fetcher: Fetcher<
+  { restaurant: GetRestaurantResult },
+  [string, string | null]
+> = async (url: string, shopeeUrl: string | null) => {
   console.log('Processing shopee url: ', shopeeUrl)
   if (!shopeeUrl) {
     return null
@@ -72,7 +86,7 @@ const fetcher = async (url: string, shopeeUrl: string | null) => {
 const Restaurant: NextPage<RestaurantProps> = ({
   restaurant: cachedRestaurant,
 }) => {
-  const { data, error } = useSWR(
+  const { data } = useSWR(
     [
       'https://foodgether-scraper.herokuapp.com/restaurants',
       cachedRestaurant?.url,
@@ -87,6 +101,7 @@ const Restaurant: NextPage<RestaurantProps> = ({
   const title = `Foodgether for ${restaurant.name}`
   const restaurantCover = restaurant.photos[restaurant.photos.length - 1]
   const restaurantCoverAlt = `foodgether ${restaurant.name} cover photo`
+  const menu = restaurant.menu
   return (
     <Box height="100%">
       <Head>
@@ -98,7 +113,7 @@ const Restaurant: NextPage<RestaurantProps> = ({
         <Flex direction="column" height="100%" gap="2">
           {!fetchedRestaurant && (
             <Flex
-              backgroundColor="orange.200"
+              backgroundColor="orange.100"
               direction="row"
               gap="2"
               height="10"
@@ -122,6 +137,91 @@ const Restaurant: NextPage<RestaurantProps> = ({
               <Stack>
                 <Heading>{restaurant.name}</Heading>
               </Stack>
+            </GridItem>
+          </Grid>
+          <Text fontSize="2xl" paddingLeft="5" color="orange.600">
+            Menu
+          </Text>
+          <Grid templateColumns="repeat(4, 1fr)" h="fit-content" gap={10}>
+            <GridItem
+              colSpan={1}
+              backgroundColor="orange.100"
+              border="1px"
+              borderColor="orange.200"
+              height="fit-content"
+            >
+              <Stack>
+                {menu.dishTypes?.map((dishType) => (
+                  <Box id={`${dishType.id}-header`} key={dishType.id}>
+                    <Text fontSize="lg" paddingLeft="1">
+                      <Link href={`#${dishType.id}`}>
+                        <ChakraLink>{dishType.name}</ChakraLink>
+                      </Link>
+                    </Text>
+                  </Box>
+                ))}
+              </Stack>
+            </GridItem>
+            <GridItem
+              colSpan={3}
+              backgroundColor="orange.100"
+              border="1px"
+              borderColor="orange.200"
+            >
+              {menu.dishTypes?.map((dishType) => (
+                <Stack
+                  id={dishType.id.toString()}
+                  marginBottom="8"
+                  key={dishType.id}
+                >
+                  <Text
+                    fontSize="2xl"
+                    paddingLeft="1"
+                    fontWeight="bold"
+                    color="orange.600"
+                  >
+                    {dishType.name}
+                  </Text>
+                  <List spacing={3}>
+                    {dishType.dishes.map((dish) => {
+                      const dishPhoto = dish.photos[dish.photos.length - 1]
+                      return (
+                        <ListItem id={dish.id.toString()} key={dish.id}>
+                          <Grid
+                            templateColumns="repeat(6, 1fr)"
+                            h="fit-content"
+                            gap={2}
+                            padding={5}
+                          >
+                            <GridItem colSpan={[5, null, 1]}>
+                              <Image
+                                src={dishPhoto.value}
+                                width={dishPhoto.width}
+                                height={dishPhoto.height}
+                                alt={dish.name}
+                              />
+                            </GridItem>
+                            <GridItem colSpan={[5, null, 4]}>
+                              <Text fontSize="lg" fontWeight="semibold">
+                                {dish.name}
+                              </Text>
+                              <Text fontSize="md">{dish.description}</Text>
+                            </GridItem>
+                            <GridItem colSpan={[5, null, 1]}>
+                              <Text fontSize="lg" fontWeight="semibold">
+                                {dish.discountPrice
+                                  ? dish.discountPrice.text
+                                  : dish.price.text}
+                              </Text>
+                            </GridItem>
+                          </Grid>
+                          <Divider borderColor="orange.200" />
+                        </ListItem>
+                      )
+                    })}
+                  </List>
+                </Stack>
+              ))}
             </GridItem>
           </Grid>
         </Flex>
