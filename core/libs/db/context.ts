af141e3637e800c587dd9949ef8b2efe5c71d862
Lam Nguyen <nguyenquanglam3008@gmail.com>
2022-08-20 18:57:24 +0700

    Seperate mock to fix import problems

--- core/libs/db/context.ts
+++ core/libs/db/context.ts
@@ -1,22 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 
-import { mockDeep, DeepMockProxy } from 'jest-mock-extended'
-import { MOCK_PRISMA } from '../config'
-
 export type Context = {
   prisma: PrismaClient
 }
 
-export type MockContext = {
-  prisma: DeepMockProxy<PrismaClient>
-}
-
-export const createPrismaContext = (): Context | MockContext => {
-  if (MOCK_PRISMA) {
-    return {
-      prisma: mockDeep<PrismaClient>(),
-    }
-  }
+export const createPrismaContext = (): Context => {
   return {
     prisma: new PrismaClient(),
   }
